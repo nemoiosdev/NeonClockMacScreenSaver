716863b5cf9626b3bff262013fa4b9da4b06ab17
Nemo Developer <nemoiosdev@gmail.com>
2020-09-22 15:19:53 +0900

    Initial Commit

---
+++ MyCustomScreenSaver/MyCustomScreenSaverView.h
@@ -0,0 +1,12 @@
+//
+//  MyCustomScreenSaverView.h
+//  MyCustomScreenSaver
+//
+//  Created by NgocThai on 9/22/20.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MyCustomScreenSaverView : ScreenSaverView
+
+@end
---
+++ MyCustomScreenSaver/MyCustomScreenSaverView.m
@@ -0,0 +1,51 @@
+//
+//  MyCustomScreenSaverView.m
+//  MyCustomScreenSaver
+//
+//  Created by NgocThai on 9/22/20.
+//
+
+#import "MyCustomScreenSaverView.h"
+
+@implementation MyCustomScreenSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
